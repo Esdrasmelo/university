62d75964ecdc8d0a761cb34ea41c8796e0f35ffe
Esdrasmelo <emelomotta@gmail.com>
2022-07-19 08:46:10 -0300

    adding pipe validation used by class-validator

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import 'reflect-metadata';
-declare const module: any;
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { APP_PORT } = process.env;
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);

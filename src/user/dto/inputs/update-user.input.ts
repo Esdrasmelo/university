c94dc85e266dfb2f9b0025d7608511d651f3925c
Esdrasmelo <emelomotta@gmail.com>
2022-07-21 09:12:26 -0300

    updating UpdateUserInput InputType

--- src/user/dto/inputs/update-user.input.ts
+++ src/user/dto/inputs/update-user.input.ts
@@ -1,8 +1,7 @@
-import { InputType, OmitType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql';
 import { CreateUserInput } from './create-user.input';
 
 @InputType()
-export class UpdateUserInput extends OmitType(CreateUserInput, [
-  'email',
-  'password',
-]) {}
+export class UpdateUserInput extends PartialType(
+  OmitType(CreateUserInput, ['email', 'password']),
+) {}
